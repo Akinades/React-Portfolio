53aa58d3f7f8ae5e0adf7ceb8e8240355f9e6b9d
Akinades <76144505+Akinades@users.noreply.github.com>
2024-01-06 08:10:01 +0700

    Update navbar.jsx

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -3,6 +3,8 @@ import logo from "../../assets//logo_nav.png";
 
 import MyLink from "./mylink";
 const Navbar = () => {
+  const linkCV =
+    "https://www.canva.com/design/DAFNzzky5WI/XVWwGwkbbCZoMk0B7Dro_Q/view?utm_content=DAFNzzky5WI&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink";
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
@@ -11,7 +13,7 @@ const Navbar = () => {
         <MyLink to="/about">About</MyLink>
         <MyLink to="/projects">Projects</MyLink>
         <MyLink to="/contact">Contact</MyLink>
-        <MyLink to="">CV</MyLink>
+        <MyLink to={linkCV}>CV</MyLink>
       </div>
     </nav>
   );
